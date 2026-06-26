366b3fc42a132219cc039683056ba608137cd50e
davilaejulia1604-art <davilaejulia1604@gmail.com>
2026-06-26 19:05:37 -0300

    Update academia.py

--- aula03/template/academia.py
+++ aula03/template/academia.py
@@ -34,7 +34,7 @@ class Academia:
 
         # TODO: troque o aviso de boas-vindas por uma chamada ao notificador.
         #   self.notificador.enviar( <para quem>, <a MESMA mensagem de boas-vindas da v1.0> )
-        ...
+        self.notificador.enviar(nome, f"Bem-vindo, {nome}! Sua matrícula foi realizada com sucesso.")
 
         print(f"Aluno {aluno['id']} matriculado.")   # TELA: este fica
 
